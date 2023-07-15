25ca4ba5bdb60d1b01f029642043c52b615c1606
James-Lee-01 <freebike01@gmail.com>
2023-07-16 03:16:24 +0800

    feat: Initialize project-Main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import StepProgress from "./components/StepProgress/StepProgress.jsx";
+import ProgressControl from "./components/ProgressControl/ProgressControl.jsx";
+import StepOne from "./components/StepOne/StepOne.jsx";
+import StepTwo from "./components/StepTwo/StepTwo.jsx";
+import StepThree from "./components/StepThree/StepThree.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="mainContainer">
+      <div className="stepContainer">
+          <StepProgress />
+        <div className="formContainer">
+            <StepOne />
+            <StepTwo />
+            <StepThree />
+        </div>
+        <div className="controlContainer">
+          <ProgressControl />
+        </div>
+      </div>
+    </main>
   );
 }
 
