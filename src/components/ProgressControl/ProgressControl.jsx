2dc91959d58fc69029770fb162e97bd9145a9378
James-Lee-01 <freebike01@gmail.com>
2023-07-29 12:28:02 +0800

    feat: icons and buttons change with step progress

--- src/components/ProgressControl/ProgressControl.jsx
+++ src/components/ProgressControl/ProgressControl.jsx
@@ -2,38 +2,25 @@ import styles from "./ProgressControl.module.css";
 import { ReactComponent as LeftArrow } from "../../icons/left-arrow.svg";
 import { ReactComponent as RightArrow } from "../../icons/right-arrow.svg";
 
-function ProgressControl() {
+function ProgressControl({ currentStep, handleClickPrev, handleClickNext}) {
   return (
-    <section class={styles.progressControlContainer}>
-      <section class={`${styles.buttonGroup} ${styles.buttonGroup1}`} data-phase="address">
-        <button class={styles.next}>
-          下一步
-          <RightArrow className={styles.arrowRight}/>
-        </button>
-      </section>
 
-      <section class={`${styles.buttonGroup} ${styles.buttonGroup2}`} data-phase="shipping">
-        <button class={styles.prev}>
+    <section className={styles.progressControlContainer}>
+      <section className={`${styles.buttonGroup} ${styles.buttonGroup2}`}>
+        <button 
+        onClick={handleClickPrev}
+        className={currentStep === 1 ? `${styles.start}` : `${styles.prev}`}>
           <LeftArrow className={styles.arrowLeft}/>
           上一步
         </button>
 
-        <button class={styles.next}>
-          下一步
-          <RightArrow className={styles.arrowRight}/>
+        <button 
+        onClick={handleClickNext}
+        className={styles.next}>
+          {currentStep === 3 ? "確認下單" : "下一步"}
+          {currentStep < 3 && <RightArrow className={styles.arrowRight}/>}
         </button>
-      </section>
-
-      <section class={`${styles.buttonGroup} ${styles.buttonGroup3}`} data-phase="credit-card">
-        <button class={styles.prev}>
-          <LeftArrow className={styles.arrowLeft}/>
-          上一步
-        </button>
-        <button class={styles.next}>
-          確認下單
-        </button>
-      </section>
-      
+      </section>     
     </section>
   )
 }
