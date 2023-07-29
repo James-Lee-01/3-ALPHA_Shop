cdd35987bfd8e134a6ffd0c07088d16ddfe745cd
James-Lee-01 <freebike01@gmail.com>
2023-07-29 12:30:25 +0800

    feat: add logic to form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import "./App.css";
+import { useState } from "react";
 import StepProgress from "./components/StepProgress/StepProgress.jsx";
 import ProgressControl from "./components/ProgressControl/ProgressControl.jsx";
 import StepOne from "./components/Steps/StepOne/StepOne.jsx";
+import StepTwo from "./components/Steps/StepTwo/StepTwo.jsx";
+import StepThree from "./components/Steps/StepThree/StepThree.jsx";
 import Cart from "./components/Cart/Cart.jsx";
 
 function App() {
+  const [currentStep, setCurrentStep] = useState(1)
+  const [shippingPrice, setShippingPrice] = useState(0)
+
+  function handleClickPrev() {
+    if (currentStep > 1) {
+      setCurrentStep(currentStep - 1)
+    }
+  }
+
+  function handleClickNext() {
+    if (currentStep < 3) {
+      setCurrentStep(currentStep + 1)
+    }
+  }
+
   return (
     <main className="mainContainer">
       <div className="stepContainer">
-          <StepProgress />
+          <StepProgress 
+            currentStep={currentStep}
+          />
         <div className="formContainer">
-            <StepOne />
+            {currentStep === 1 && <StepOne />}
+            {currentStep === 2 && <StepTwo setShippingPrice={setShippingPrice}/>}
+            {currentStep === 3 && <StepThree />}
         </div>
         <div className="controlContainer">
-          <ProgressControl />
+          <ProgressControl
+            currentStep={currentStep}
+            handleClickPrev={handleClickPrev}
+            handleClickNext={handleClickNext}
+          />
         </div>
       </div>
       <div className="cartSideContainer">
-      <Cart />
+      <Cart shippingPrice={shippingPrice}/>
       </div>
     </main>
   );
