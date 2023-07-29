dfd9dc4ccc2048266b24fb597e114968fbf4e996
James-Lee-01 <freebike01@gmail.com>
2023-07-29 12:21:13 +0800

    feat: shipping feed switch

--- src/components/Steps/StepTwo/StepTwo.jsx
+++ src/components/Steps/StepTwo/StepTwo.jsx
@@ -1,46 +1,57 @@
 import styles from "./StepTwo.module.css";
+import { useState } from "react";
 
-function StepTwo() {
+function StepTwo({setShippingPrice}) {
+  const [shipping, setShipping] = useState('standard')
+  const handleSelectedShipping = e => setShipping(e.target.id)
+
+  function handleRadioChange(price) {
+    setShippingPrice(price)
+  }
+
   return (
     <form data-phase="shipping">
-
-      <h3 class={styles.formTitle}>運送方式</h3>
-
-      <section class={styles.formBody} >
-        <label class={styles.radioGroup } data-price="0">
-          
-          <input id={styles.inputStyle} type="radio" name="shipping"  checked/>
-          
-          <div class={styles.radioInfo}>
+      <h3 className={styles.formTitle}>運送方式</h3>
+      <section className={styles.formBody}>
+        <label className={styles.radioGroup} onClick={() => handleRadioChange(0)}>
+          <input
+            className={styles.input}
+            id="standard"
+            type="radio"
+            name="shipping"
+            checked={shipping === "standard"}
+            onChange={handleSelectedShipping}
+          />
+          <div className={styles.radioInfo}>
             <div>
-              <div class={styles.text}>標準運送</div>
-              <div class={styles.price}></div>
+              <div className={styles.text}>標準運送</div>
+              <div className={styles.price}></div>
             </div>
-
-            <div class={styles.period}>約 3~7 個工作天</div>
+            <div className={styles.period}>約 3~7 個工作天</div>
           </div>
-          
-          <div class={styles.radioBoxBorder}>免費</div>
+          <div className={styles.radioBoxBorder}>免費</div>
         </label>
-        
-        <label class={styles.radioGroup} data-price="500">
-
-          <input id={styles.inputStyle} type="radio" name="shipping" />
-
-          <div class={styles.radioInfo}>
+        <label className={styles.radioGroup} onClick={() => handleRadioChange(500)}>
+          <input
+            className={styles.input}
+            id="DHL"
+            type="radio"
+            name="shipping"
+            checked={shipping === "DHL"}
+            onChange={handleSelectedShipping}
+          />
+          <div className={styles.radioInfo}>
             <div>
-              <div class={styles.text}>DHL 貨運</div>
-              <div class={styles.price}></div>
+              <div className={styles.text}>DHL 貨運</div>
+              <div className={styles.price}></div>
             </div>
-
-            <div class={styles.period }>48 小時內送達</div>
+            <div className={styles.period}>48 小時內送達</div>
           </div>
-
-          <div class={styles.radioBoxBorder}>$500</div>
+          <div className={styles.radioBoxBorder}>$500</div>
         </label>
       </section>
     </form>
-  )
+  );
 }
 
 export default StepTwo;
