ce1b9c7ad1d545f88e62cdf9be622cc3db55016a
James-Lee-01 <freebike01@gmail.com>
2023-08-04 22:20:10 +0800

    feat: add name tag

--- src/components/Steps/StepOne/StepOne.jsx
+++ src/components/Steps/StepOne/StepOne.jsx
@@ -3,7 +3,7 @@ import stepOneData from "./stepOneData.js"
 
 function GenderData({data}) {
   return (
-    <select className={styles.selectStyle}>
+    <select className={styles.selectStyle} name="addressGender">
       {data.map((gender) => {
         return (
           <option key={gender.value} value={gender.value}>
@@ -12,12 +12,12 @@ function GenderData({data}) {
         );
       })}
     </select>
-  )
+  );
 }
 
 function CityData({data}) {
   return (
-    <select className={styles.selectStyle} required>
+    <select className={styles.selectStyle} name="addressGender" required>
       {data.map((city) => {
         return (
           <option key={city.value} value={city.value}>
@@ -26,57 +26,77 @@ function CityData({data}) {
         );
       })}
     </select>
-  )
+  );
 }
 
 function StepOne() {
   return (
     <>
-    <form data-phase="address">
-      <h3 className={styles.formTitle}>寄送地址</h3>
-      <section className={styles.formBody}>
-
-        <div className={styles.formItem}>
-          <div className={styles.inputGroupL1}> 
-            <div className={styles.inputLabel}>稱謂</div>
-            <div className={styles.selectContainer}>
-              <GenderData data={stepOneData.gender} />
+      <form data-phase="address">
+        <h3 className={styles.formTitle}>寄送地址</h3>
+        <section className={styles.formBody}>
+          <div className={styles.formItem}>
+            <div className={styles.inputGroupL1}>
+              <div className={styles.inputLabel}>
+                稱謂
+              </div>
+              <div className={styles.selectContainer}>
+                <GenderData data={stepOneData.gender} />
+              </div>
             </div>
-          </div>          
-          <div className={styles.inputGroupL2}>
-            <div className={styles.inputLabel}>姓名</div>
-            <input className={styles.inputStyle} type="text" placeholder="請輸入姓名"/>
-          </div>
-        </div>
-
-
-        <div className={styles.formItem}>
-          <div className={styles.inputGroupL2}>
-            <div className={styles.inputLabel}>電話</div>
-            <input className={styles.inputStyle} type="tel" placeholder="請輸入行動電話"/>
-          </div>
-          <div className={styles.inputGroupL2}>
-            <div className={styles.inputLabel}>Email</div>
-            <input className={styles.inputStyle} type="email" placeholder="請輸入電子郵件"/>
-          </div>
-        </div>
-
-        <div className={styles.formItem}>
-          <div className={styles.inputGroupL1}>
-            <div className={styles.inputLabel}>縣市</div>          
-            <div className={styles.selectContainer}>           
-              <CityData data={stepOneData.city}/>             
+            <div className={styles.inputGroupL2}>
+              <div className={styles.inputLabel}>姓名</div>
+              <input
+                className={styles.inputStyle}
+                name="addressName"
+                type="text"
+                placeholder="請輸入姓名"
+              />
             </div>
           </div>
-          <div className={styles.inputGroupL2}>
-            <div className={styles.inputLabel}>地址</div>
-            <input className={styles.inputStyle} type="text" placeholder="請輸入地址"/>
+
+          <div className={styles.formItem}>
+            <div className={styles.inputGroupL2}>
+              <div className={styles.inputLabel}>電話</div>
+              <input
+                className={styles.inputStyle}
+                name="addressPhone"
+                type="tel"
+                placeholder="請輸入行動電話"
+              />
+            </div>
+            <div className={styles.inputGroupL2}>
+              <div className={styles.inputLabel}>Email</div>
+              <input
+                className={styles.inputStyle}
+                name="addressMail"
+                type="email"
+                placeholder="請輸入電子郵件"
+              />
+            </div>
           </div>
-        </div>
-      </section>
-    </form>
+
+          <div className={styles.formItem}>
+            <div className={styles.inputGroupL1}>
+              <div className={styles.inputLabel}>縣市</div>
+              <div className={styles.selectContainer}>
+                <CityData data={stepOneData.city} />
+              </div>
+            </div>
+            <div className={styles.inputGroupL2}>
+              <div className={styles.inputLabel}>地址</div>
+              <input
+                className={styles.inputStyle}
+                name="addressAddress"
+                type="text"
+                placeholder="請輸入地址"
+              />
+            </div>
+          </div>
+        </section>
+      </form>
     </>
-  )
+  );
 }
 
 export default StepOne;
