961f59d535d35ae371e995d571c35e68d1b81c13
James-Lee-01 <freebike01@gmail.com>
2023-08-04 22:22:51 +0800

    feat: adjust logic and use useContext

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,19 +1,21 @@
-import styles from "./Cart.module.css"
-import { useState } from "react"
-import { itemData } from "./itemData.js"
-import { ReactComponent as PlusBtn } from "../../icons/plus.svg"
-import { ReactComponent as MinusBtn } from "../../icons/minus.svg"
+import styles from "./Cart.module.css";
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext.js";
+import { ReactComponent as PlusBtn } from "../../icons/plus.svg";
+import { ReactComponent as MinusBtn } from "../../icons/minus.svg";
 
-function Product({item, handleMinus, handlePlus}) {
+function Product({item}) {
+  const {handleMinus, handlePlus} = useContext(CartContext);
+
   return (
     <div className={styles.productContainer} key={item.id}>
-      <img className={styles.imgContainer} alt={item.name} src={item.image}/>
+      <img className={styles.imgContainer} alt={item.name} src={item.image} />
       <div className={styles.productInfo}>
         <div className={styles.productName}>{item.name}</div>
         <div className={styles.productControlContainer}>
           <div className={styles.productControl}>
-            <MinusBtn 
-              className={styles.productAction} 
+            <MinusBtn
+              className={styles.productAction}
               onClick={() => handleMinus(item.id)}
             />
             <span className={styles.productCount}>{item.quantity}</span>
@@ -29,73 +31,27 @@ function Product({item, handleMinus, handlePlus}) {
   );
 }
 
-function CartItem({ shippingPrice }) {
-  const [currentProduct, setCurrentProduct] = useState(itemData);
-
-  function handleClickPlus(id) {
-    const newCurrentProduct = currentProduct.map((product) => {
-      if (product.id === id) {
-        return {
-          ...product,
-          quantity: product.quantity + 1,
-        };
-      } else {
-        return product;
-      }
-    });
-    setCurrentProduct(newCurrentProduct)
-  }
-
-  function handleClickMinus(id) {
-    const newCurrentProduct = currentProduct.map((product) => {
-      if (product.id === id && product.quantity > 0) {
-        return {
-          ...product,
-          quantity: product.quantity - 1
-        }
-      } else {
-        return product
-      }
-    })
-    setCurrentProduct(newCurrentProduct.filter(product => product.quantity !== 0))
-  }
-
-  function addTotalPrice() {
-    const totalPrice = currentProduct.reduce((total, product) => {
-      return total + (product.quantity * product.price)
-    }, 0)
-
-    if (totalPrice !== 0) {
-      return totalPrice + shippingPrice;
-    } else {
-      return 0
-    }
-    
-  }
+function CartItem() {
+  const {currentProduct, allPrice, shippingPrice} = useContext(CartContext);
 
   return (
     <section className={styles.productList}>
       {currentProduct.map((item) => (
-        <Product
-          item={item}
-          key={item.id}
-          handleMinus={handleClickMinus}
-          handlePlus={handleClickPlus}
-        />
+        <Product item={item} key={item.id} />
       ))}
 
       <section className={styles.cartInfo}>
         <div className={styles.text}>運費</div>
         <div className={styles.price}>
-          {shippingPrice === 0 ? "免費" : "$"+shippingPrice}
+          {shippingPrice === 0 ? "免費" : "$" + shippingPrice}
         </div>
       </section>
       <section className={styles.cartInfo}>
         <div className={styles.text}>小計</div>
-        <div className={styles.price}>${addTotalPrice()}</div>
+        <div className={styles.price}>${allPrice}</div>
       </section>
     </section>
   );
 }
 
-export default CartItem
+export default CartItem;
